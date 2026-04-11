de62968309dbb4810c6252154778db7084276749
nicandromico-maker <nicandro.mico@gmail.com>
2026-04-11 19:29:51 +0800

    Add project files.

---
+++ Linguae/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Linguae
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxCamera = new System.Windows.Forms.PictureBox();
+            this.labelSubtitle = new System.Windows.Forms.Label();
+            this.textBoxChat = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.pictureBoxSign = new System.Windows.Forms.PictureBox();
+            this.pictureBoxInfo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCamera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSign)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxCamera
+            // 
+            this.pictureBoxCamera.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBoxCamera.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxCamera.Name = "pictureBoxCamera";
+            this.pictureBoxCamera.Size = new System.Drawing.Size(1095, 418);
+            this.pictureBoxCamera.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxCamera.TabIndex = 0;
+            this.pictureBoxCamera.TabStop = false;
+            this.pictureBoxCamera.Click += new System.EventHandler(this.pictureBoxCamera_Click);
+            // 
+            // labelSubtitle
+            // 
+            this.labelSubtitle.AutoSize = true;
+            this.labelSubtitle.BackColor = System.Drawing.Color.Black;
+            this.labelSubtitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelSubtitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSubtitle.ForeColor = System.Drawing.Color.White;
+            this.labelSubtitle.Location = new System.Drawing.Point(0, 418);
+            this.labelSubtitle.Name = "labelSubtitle";
+            this.labelSubtitle.Size = new System.Drawing.Size(453, 36);
+            this.labelSubtitle.TabIndex = 1;
+            this.labelSubtitle.Text = "Detected sign will appear here.";
+            // 
+            // textBoxChat
+            // 
+            this.textBoxChat.Location = new System.Drawing.Point(6, 476);
+            this.textBoxChat.Name = "textBoxChat";
+            this.textBoxChat.Size = new System.Drawing.Size(420, 22);
+            this.textBoxChat.TabIndex = 2;
+            this.textBoxChat.TextChanged += new System.EventHandler(this.textBoxChat_TextChanged);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(449, 476);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 3;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // pictureBoxSign
+            // 
+            this.pictureBoxSign.Location = new System.Drawing.Point(901, 418);
+            this.pictureBoxSign.Name = "pictureBoxSign";
+            this.pictureBoxSign.Size = new System.Drawing.Size(194, 146);
+            this.pictureBoxSign.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSign.TabIndex = 4;
+            this.pictureBoxSign.TabStop = false;
+            // 
+            // pictureBoxInfo
+            // 
+            this.pictureBoxInfo.Location = new System.Drawing.Point(705, 418);
+            this.pictureBoxInfo.Name = "pictureBoxInfo";
+            this.pictureBoxInfo.Size = new System.Drawing.Size(190, 146);
+            this.pictureBoxInfo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxInfo.TabIndex = 5;
+            this.pictureBoxInfo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1095, 565);
+            this.Controls.Add(this.pictureBoxInfo);
+            this.Controls.Add(this.pictureBoxSign);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxChat);
+            this.Controls.Add(this.labelSubtitle);
+            this.Controls.Add(this.pictureBoxCamera);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCamera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSign)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxCamera;
+        private System.Windows.Forms.Label labelSubtitle;
+        private System.Windows.Forms.TextBox textBoxChat;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.PictureBox pictureBoxSign;
+        private System.Windows.Forms.PictureBox pictureBoxInfo;
+    }
+}
+
---
+++ Linguae/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Linguae
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxCamera_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+
+            string userText = textBoxChat.Text;
+
+            labelSubtitle.Text = userText;
+
+            ShowSign(userText);
+
+            textBoxChat.Clear();
+        }
+        private void ShowSign(string text)
+        {
+            string path = "";
+
+            switch (text.ToLower())
+            {
+                case "hello":
+                    path = "Signs/hello.gif";
+                    break;
+
+                case "help":
+                    path = "Signs/help.gif";
+                    break;
+
+                default:
+                    path = "Signs/default.gif";
+                    break;
+            }
+
+            string fullPath = System.IO.Path.Combine(Application.StartupPath, path);
+            if (System.IO.File.Exists(fullPath))
+            {
+                pictureBoxSign.Image = Image.FromFile(fullPath);
+            }
+            else
+            {
+                MessageBox.Show("File not found: " + fullPath);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxChat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
